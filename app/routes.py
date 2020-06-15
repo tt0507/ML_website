c03551562157e2030363ee5b09eb192b0c9b32e7
Toshi Tokuyama <47758668+tt0507@users.noreply.github.com>
2020-06-15 12:37:37 +0900

    start basic structure

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
+from flask import render_template, url_for, redirect
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello world"
+    return render_template('index.html', title='Website', header="Machine Learning Projects")
+
+
+@app.route('/project')
+def project():
+    return render_template('project.html', title='Projects', header="Project List")
