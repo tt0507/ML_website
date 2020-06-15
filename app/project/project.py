0a92092e2df691bf48fc83c6321b28f5e6ae8525
Toshi Tokuyama <47758668+tt0507@users.noreply.github.com>
2020-06-16 02:04:44 +0900

    connected using sqlalchemy

--- app/project/project.py
+++ app/project/project.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 import app.web_scraping as web_scraping
 import pandas as pd
+from sqlalchemy import create_engine
+import os
 
 project_bp = Blueprint(
     'project_bp', __name__,
@@ -8,9 +10,14 @@ project_bp = Blueprint(
     static_folder='static'
 )
 
+url = os.environ.get("MYSQL_URL")
+database = create_engine(url)
+mysql = database.connect()
+mysql.execute("use ml_website_database")
+
 
 @project_bp.route('/project/time_forecast')
 def time_forecast():
     # stock_data = web_scraping.get_stock_price()
-    stock_data = [[0, 0], [1, 1], [2, 2]]
-    return render_template('time_forecast.html', stock_data=stock_data)
+    data = mysql.execute("SELECT * FROM dow_jones_industrial")
+    return render_template('time_forecast.html', stock_data=data)
--- config.py
+++ config.py
@@ -3,9 +3,3 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
-
-    # mysql
-    MYSQL_HOST = os.environ.get('MYSQL_HOST')
-    MYSQL_USER = os.environ.get('MYSQL_USER')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-    MYSQL_DB = os.environ.get('MYSQL_DB')
