b223f8b31c1ce0c7b5b3cecfa3767e77c06870ed
Toshi Tokuyama <47758668+tt0507@users.noreply.github.com>
2020-06-15 14:13:34 +0900

    create project workflow

--- app/routes.py
+++ app/routes.py
@@ -9,4 +9,16 @@ def index():
 
 @app.route('/project')
 def project():
-    return render_template('project.html', title='Projects', header="Project List")
+    finance = [
+        {
+            'title': 'Time Series Forecasting',
+            'description': 'Time series forecasting for Dow Jones Industrial Average, S&P 500, and NASDAQ Composite',
+            'link': '/project/time_forecast'
+        }
+    ]
+    return render_template('project.html', title='Projects', header="Project List", finance=finance)
+
+
+@app.route('/project/time_forecast')
+def project_time_forecast():
+    return render_template('project/time_forecast.html')
