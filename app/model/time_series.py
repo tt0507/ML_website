9c41643bcf349fa2521596cee4f93a41a74aee8b
Toshi Tokuyama <47758668+tt0507@users.noreply.github.com>
2020-07-21 23:24:08 +0900

    made initial change for transition to google cloud platform

--- app/model/time_series.py
+++ app/model/time_series.py
@@ -1,160 +1,119 @@
+import googleapiclient.discovery
 import json
-from pathlib import Path
-
+import os
 import numpy as np
-import tensorflow as tf
-from pandas.io.json import json_normalize
-from sklearn.preprocessing import StandardScaler
-from tensorflow import keras
+from sqlalchemy import create_engine
 
 
-def run_time_series(data1, data2, data3):
+def predict_next_prices():
     """
-    run RNN time series model
-    :param data1: dji data
-    :param data2: sp500 data
-    :param data3: nasdaq data
+    Predict time series using Google Cloud Platform
+    :return: predicted values
+    """
+    project_id = "tidal-eon-283914"
+
+    # dji model
+    dji_model_id = "dji_model"
+    dji_version = "v0001"
+    dji_model_path = "projects/{}/models/{}/versions/{}".format(project_id, dji_model_id, dji_version)
+
+    # sp500 model
+    sp500_model_id = "sp500_model"
+    sp500_version = "v0001"
+    sp500_model_path = "projects/{}/models/{}/versions/{}".format(project_id, sp500_model_id, sp500_version)
+
+    # nasdaq model
+    nasdaq_model_id = "nasdaq_model"
+    nasdaq_version = "v0001"
+    nasdaq_model_path = "projects/{}/models/{}/versions/{}".format(project_id, nasdaq_model_id, nasdaq_version)
+
+    # get the projects
+    ml_resource = googleapiclient.discovery.build("ml", "v1").projects()
+
+    # get data for prediction
+    dji_data, sp500_data, nasdaq_data = get_data()
+
+    # predict
+    dji_predict = predict(dji_data, ml_resource, dji_model_path)
+    sp500_predict = predict(sp500_data, ml_resource, sp500_model_path)
+    nasdaq_predict = predict(nasdaq_data, ml_resource, nasdaq_model_path)
+
+    return dji_predict, sp500_predict, nasdaq_predict
+
+
+def predict(X, resource, model_path):
+    """
+    Predict next 10 values of time series model
+    :param X: data used for prediction
+    :param resource: resource object used to call the prediction service
+    :param model_path: path for the model
+    :return: predicted value as numpy array
+    """
+
+    input_data_json = {
+        "signature_name": "serving_default",
+        "instances": X.tolist()
+    }
+
+    request = resource.predict(name=model_path, body=input_data_json)
+    response = request.execute()
+
+    if "error" in response:
+        raise RuntimeError(response["error"])
+
+    # TODO: check for loop for returning prediction
+    return np.array([pred for pred in response["prediction"]])
+
+
+def get_data():
+    """
+
     :return:
     """
-    # import json data and change to dataframe
-    dji = json_normalize(json.loads(data1))
-    sp500 = json_normalize(json.loads(data2))
-    nasdaq = json_normalize(json.loads(data3))
+    url = os.environ.get("MYSQL_URL")
+    database = create_engine(url)
+    mysql = database.connect()
+    mysql.execute("use ml_website_database")
 
-    # set index to date
-    dji = dji.set_index('date')
-    sp500 = sp500.set_index('date')
-    nasdaq = nasdaq.set_index('date')
+    # get last 10 value from database
+    dji_data = mysql.execute("SELECT * FROM dow_jones_industrial").fetchall()
+    sp500_data = mysql.execute("SELECT * FROM sp500").fetchall()
+    nasdaq_data = mysql.execute("SELECT * FROM nasdaq").fetchall()
 
-    assert len(dji) == len(sp500)
-    assert len(dji) == len(nasdaq)
-    assert len(sp500) == len(nasdaq)
+    header = ("date", "open", "high", "low", "close", "adj_close", "volume")
+    dji_json = jsonify_data(header, dji_data)
+    sp500_json = jsonify_data(header, sp500_data)
+    nasdaq_json = jsonify_data(header, nasdaq_data)
 
-    # Standarize the values
-    scale = StandardScaler()
-    dji_standarized = scale.fit_transform(dji.values)
-    sp500_standarized = scale.fit_transform(sp500.values)
-    nasdaq_standarized = scale.fit_transform(nasdaq.values)
+    # TODO: change data into numpy array
 
-    # split to train data and label data
-    dji_data, dji_label = train_data(dji_standarized, 4, 20)
-    sp500_data, sp500_label = train_data(sp500_standarized, 4, 20)
-    nasdaq_data, nasdaq_label = train_data(nasdaq_standarized, 4, 20)
-
-    # check shape of training data
-    assert dji_data.shape == (len(dji_standarized) - 20, 20, 6)
-    assert sp500_data.shape == (len(sp500_standarized) - 20, 20, 6)
-    assert nasdaq_data.shape == (len(nasdaq_standarized) - 20, 20, 6)
-
-    # check shape of label data
-    assert dji_label.shape == (len(dji_standarized) - 20,)
-    assert sp500_label.shape == (len(sp500_standarized) - 20,)
-    assert nasdaq_label.shape == (len(nasdaq_standarized) - 20,)
-
-    # run model and get predicted value
-    dji_predict_stan = run_model("app/model/saved_model/dji_model.h5", dji_data, dji_label, 10)
-    sp500_predict_stan = run_model("app/model/saved_model/sp500_model.h5", sp500_data, sp500_label, 10)
-    nasdaq_predict_stan = run_model("app/model/saved_model/nasdaq_model.h5", nasdaq_data, nasdaq_label, 10)
-
-    # get unstandarized version of predicted value
-    dji_predict = unstandarize(dji_predict_stan, dji['adj_close'])
-    sp500_predict = unstandarize(sp500_predict_stan, sp500['adj_close'])
-    nasdaq_predict = unstandarize(nasdaq_predict_stan, nasdaq['adj_close'])
-
-    dji_predict_json = json.dumps(dict(zip(list(range(len(dji_predict))), dji_predict.tolist())))
-    sp500_predict_json = json.dumps(dict(zip(list(range(len(sp500_predict))), sp500_predict.tolist())))
-    nasdaq_predict_json = json.dumps(dict(zip(list(range(len(nasdaq_predict))), nasdaq_predict.tolist())))
-
-    return dji_predict_json, sp500_predict_json, nasdaq_predict_json
+    return dji_json, sp500_json, nasdaq_json
 
 
-def train_data(dataset, target_index, start_index):
+def jsonify_data(header, data):
     """
-    split data in 3D array of shape (len(dataset), start_index, # of columns)
-    :param dataset: dataset to partition data
-    :param target_index: column which to predict
-    :param start_index: index to split the data to fit RNN input shape
-    :return: train data and label data in numpy array
+    Turn mysql data into json format
+    :param header: header of database
+    :param data: data of database
+    :return: return json version of data
     """
-    data = []
-    labels = []
-    target = dataset[:, target_index]
+    json_data = []
 
-    # model will be given 20 prices
-    start_index = start_index
-
-    for i in range(start_index, len(dataset)):
-        indices = range(i - start_index, i)
-        data.append(dataset[indices])
-        labels.append(target[i])
-
-    return np.array(data), np.array(labels)
+    for row in data:
+        json_data.append({
+            header[0]: str(row[0]), header[1]: row[1], header[2]: row[2], header[3]: row[3], header[4]: row[4],
+            header[5]: row[5], header[6]: row[6]
+        })
+    return json.dumps(json_data)
 
 
-def predict_next_prices(train, label, model_path, predict_num):
+def get_visualization_data():
     """
-    Train RNN model on training data and label data
-    :param model_path:
-    :param predict_num:
-    :param train: train data
-    :param label: label data
+
     :return:
     """
-    model = keras.models.Sequential([
-        keras.layers.LSTM(100, return_sequences=True, input_shape=train.shape[-2:]),
-        keras.layers.LSTM(50, return_sequences=True),
-        keras.layers.LSTM(25),
-        keras.layers.Dense(1)
-    ])
-
-    # fit model
-    callback = keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
-    checkpoint_cb = keras.callbacks.ModelCheckpoint(model_path, save_best_only=True)
-    model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='mae')
-    model.fit(train, label, epochs=100, callbacks=[callback, checkpoint_cb])
-    model.save(model_path)
-
-    # predict data
-    predict_data = train[-predict_num:]
-    predict = model.predict(predict_data)
-
-    return predict
-
-
-def run_model(path, data, label, predict_length):
-    """
-    Run RNN model
-    :param path: path to .h5 model file
-    :param data: train data
-    :param label: target data
-    :param predict_length: amount of days to predict
-    :return: predicted value for predict_length days
-    """
-    model_file = Path(path)
-    if model_file.is_file():
-        predict_dji = data[-predict_length:]
-        model = keras.models.load_model(path)
-        predicted_value = model.predict(predict_dji)
-    else:
-        predicted_value = predict_next_prices(data, label, path,
-                                              predict_num=predict_length)
-    assert predicted_value.shape == (predict_length, 1)
-
-    return predicted_value
-
-
-def unstandarize(predict, target_data):
-    """
-    unstandarize the predict column which represents a Z-score value
-    :param predict:
-    :param target_data:
-    :return:
-    """
-    mean = target_data.mean()
-    std = target_data.std()
-    unstandarized_arr = predict * std + mean
-    return unstandarized_arr
+    pass
 
 
 if __name__ == "__main__":
-    run_time_series()
+    predict_next_prices()
--- app/project/project.py
+++ app/project/project.py
@@ -1,11 +1,6 @@
-import json
-import os
-
 from flask import Blueprint, render_template, request
-from sqlalchemy import create_engine
-
 from app.data.stock_data import process
-from app.model.time_series import run_time_series
+from app.model.time_series import predict_next_prices, get_visualization_data
 from app.project.forms import RecommendationForm
 from app.model.recommendation_cosine import get_recommendation
 
@@ -15,50 +10,21 @@ project_bp = Blueprint(
     static_folder='static'
 )
 
-url = os.environ.get("MYSQL_URL")
-database = create_engine(url)
-mysql = database.connect()
-mysql.execute("use ml_website_database")
-
 
 @project_bp.route('/project/time_forecast')
 def time_forecast():
     # insert into database
     process()
 
-    # get value from database
-    dji_data = mysql.execute("SELECT * FROM dow_jones_industrial").fetchall()
-    sp500_data = mysql.execute("SELECT * FROM sp500").fetchall()
-    nasdaq_data = mysql.execute("SELECT * FROM nasdaq").fetchall()
+    # get predicted data
+    dji_predict_json, sp500_predict_json, nasdaq_predict_json = predict_next_prices()
 
-    header = ("date", "open", "high", "low", "close", "adj_close", "volume")
-    dji_json = jsonify_data(header, dji_data)
-    sp500_json = jsonify_data(header, sp500_data)
-    nasdaq_json = jsonify_data(header, nasdaq_data)
-
-    dji_predict_json, sp500_predict_json, nasdaq_predict_json = run_time_series(dji_json, sp500_json, nasdaq_json)
+    # get data for visualization
+    dji_json, sp500_json, nasdaq_json = get_visualization_data()
 
     return render_template('time_forecast.html', dji_json=dji_json, sp500_json=sp500_json, nasdaq_json=nasdaq_json,
                            dji_predict_json=dji_predict_json, sp500_predict_json=sp500_predict_json,
                            nasdaq_predict_json=nasdaq_predict_json)
-
-
-def jsonify_data(header, data):
-    """
-    Turn mysql data into json format
-    :param header: header of database
-    :param data: data of database
-    :return: return json version of data
-    """
-    json_data = []
-    # for row in data:
-    #     json_data.append(dict(zip(header, row)))
-    for row in data:
-        json_data.append({
-            header[0]: str(row[0]), header[1]: row[1], header[2]: row[2], header[3]: row[3], header[4]: row[4],
-            header[5]: row[5], header[6]: row[6]
-        })
-    return json.dumps(json_data)
 
 
 @project_bp.route('/project/rs_cosine', methods=['GET', 'POST'])
